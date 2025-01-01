ca4186c1d734c90c4cecc402a4449e3afec88a2b
Tim Linsenmayer <dhbw@tim.it.com>
2025-01-01 16:43:59 +0100

    Add clear foreground to 1-1

--- Kubelab/round-1/0-name/foreground.sh
+++ Kubelab/round-1/0-name/foreground.sh
@@ -1,10 +1,6 @@
-#!/bin/bash
-
-# Wait for the prompt script to be created by background.sh
 while [ ! -f "/tmp/kubelab/prompt.sh" ]; do
     sleep 1
 done
 
-# Make it executable and run it
 chmod +x /tmp/kubelab/prompt.sh
 /tmp/kubelab/prompt.sh
